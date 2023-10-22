4ab45f7cb4dbbb1f3a566d27ca0f4fc4f510e4c3
yantheLousada <yanthe96@hotmail.com>
2023-10-22 12:18:30 -0300

    desafio poo

---
+++ iPhone.java
@@ -0,0 +1,5 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    // Implemente os métodos das interfaces ReprodutorMusical, AparelhoTelefonico e NavegadorInternet aqui.
+    // ...
+}
+
